988da90981dff2172dd6fd8bfb9a5ad52b2c9270
ChairulNew <chairularul010805@gmail.com>
2025-06-26 14:50:21 +0700

    penyesuaian pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,23 @@
-// m14- pertama
+// latihan 14
+// import 'package:flutter/material.dart';
+// import 'package:minggu_12/latihan14/main_shared_preferences.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(primarySwatch: Colors.blue),
+//       home: SignInScreen(),
+//     );
+//   }
+// }
+
+// m14- praktek - 1
 
 // import 'package:flutter/material.dart';
 // import 'package:minggu_12/M14/product_screen.dart';
@@ -24,8 +43,9 @@
 //   }
 // }
 
-// m14 - kedua
+// m14 - praktek - 1
 import 'package:flutter/material.dart';
+import 'package:minggu_12/latihan14/main_shared_preferences.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:minggu_12/M14-2/signin_app_screen.dart';
 import 'package:minggu_12/M14-2/home_app_screen.dart';
